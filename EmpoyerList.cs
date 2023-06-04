85f10bdccfb54f3ab59aff3686dc740a146ea88a
Nikita Sychev <nikitasychev@inbox.ru>
2023-06-04 22:05:06 +0500

    осталось: базу данных допилить + распечатка графиков и метод + наследование

--- EmpoyerList.cs
+++ EmpoyerList.cs
@@ -3,75 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
 
-// мысли проработать класс, потому что мне не нравится его струкура
-// НЕ ДОДЕЛАНО ! 
+
 
 namespace Employer_LIST_HOMEWORK
 {
     public class EmployerList
     {
-        private int age;
+  
         private string name;
         private string sex;
         private DateTime DateOfBirth;
-        public  EmployerList()
-        {   
-            age = 0;
+        private string position;
+        public EmployerList()
+        {
             name = "";
             sex = "M";
-            DateTime DateOfBirth = new DateTime(1950, 1, 1); //год месяц день
+          DateTime DateOfBirth = new DateTime(1950, 1, 1); //год месяц день
         }
-        public EmployerList(int _age, string _name)
-        {
-            if (this.SetAge(_age))
-            {
-                this.age = _age;
-                this.name = _name;
-            }
-        }
-        public int Age { get { return age; } }
         public string Name { get { return name; } }
-        public bool SetAge(int _age)
-        {
-            bool flag = false;
-            if (_age >= 14 & _age <= 100)
-            {
-                age = _age;
-                flag = true;
-            }
-            return flag;
-        }
         public void SetName(string _name)
         {
             this.name = _name;
         }
-        public void addEmployer () // метод добавления сотрудника
+        public string Sex { get { return sex; } }
+        public void SetSex(string _sex)
         {
-            string name, sex;
-            
-            DateTime DateOfBirth;
-            Console.WriteLine("Введите имя сотрудника");
-            name = Console.ReadLine();
-            do // сделали цикл ввода
-            {
-                Console.WriteLine("Введите пол сотрудника: M - мужчина или W - женщика");
-                sex = Console.ReadLine();
-            } while (sex.ToLower() != "m" && sex.ToLower() != "w"); 
-          
-            try // проверка на корректность
-            {
-                Console.WriteLine("Введите дату рождения в формате: 1990.12.03");
-                string dateInput = Console.ReadLine();
-                var tmp = dateInput.Split('.');
-                DateOfBirth = new DateTime(int.Parse(tmp[0]), int.Parse(tmp[1]), int.Parse(tmp[2]));
-            }
-            catch
-            {
-                Console.WriteLine("Некорректный ввод даты");
-            }
-            
-
+            this.sex = _sex;
         }
+        public string Positon { get { return position; } }
+        public void SetPosition(string _position)
+        {
+            this.position = _position;
+        }
+
+
     }
-}
+    }
+
--- Program.cs
+++ Program.cs
@@ -3,18 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
 
 namespace Employer_LIST_HOMEWORK
 {
     internal class Program
     {
 
+        static EmployerList addEmployer(EmployerList emp) // метод добавления сотрудника
+        {
+            string _name, _sex;
+            SQLiteCommand command = null; // создали переменную для создания команд в базе данных
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=employerList.db; "); //создали объект класса для БД
+            connection.Open(); // открыли соединение БД
+
+            Console.WriteLine("Введите имя сотрудника");
+            _name = Console.ReadLine();
+            emp.SetName(_name);
+            do // сделали цикл ввода
+            {
+                Console.WriteLine("Введите пол сотрудника: M - мужчина или W - женщика");
+                _sex = Console.ReadLine();
+            } while (_sex.ToLower() != "m" && _sex.ToLower() != "w");
+            emp.SetSex(_sex);
+            Console.WriteLine("Введите год рождения в формате: 1990");
+            int _year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц рождения в формате: 01");
+            int _month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день рождения в формате: 01");
+            int _day = int.Parse(Console.ReadLine());
+            DateTime DateOfBirth = new DateTime(_year, _month, _day);
+
+            Console.WriteLine($"Мы записали сотрудника {_name}, пол: {_sex} и датой рождения: {DateOfBirth} в базу данных.");
+
+            command.CommandText = $"INSERT INTO People (Имя, Пол, Дата рождения) VALUES ({_name}, {_sex}, {DateOfBirth})";
+            command.Parameters.AddWithValue("name", _name);
+            command.Parameters.AddWithValue("sex", _sex);
+            command.Parameters.AddWithValue("Birth", DateOfBirth);
+            connection.Close();
+            return emp;
+        }
+
         static void Main(string[] args)
         {
             EmployerList employer1 = new EmployerList();
-            employer1.addEmployer();
+            addEmployer(employer1);
+            Console.WriteLine($"Имя {employer1.Name}");
 
-            // завтра записать в базу данных и освоить это дело
+            // завтра записать в базу данных и освоить это дел
+           
         }
     }
 }
