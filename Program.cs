8afdd4826af88a9abf6c71111310c3fd5acaabc0
Nikita Sychev <nikitasychev@inbox.ru>
2023-06-08 21:04:03 +0500

    adsda

--- Program.cs
+++ Program.cs
@@ -10,36 +10,17 @@ namespace Employer_LIST_HOMEWORK
 {
     internal class Program
     {
-
-        static public void DBConnect(string _DBname)
-        {
-            SQLiteCommand command = new SQLiteCommand();
-            SQLiteConnection connection = new SQLiteConnection();
-            try
-            {
-                connection = new SQLiteConnection("Data Source=" + _DBname + ";Version=3; FailIfMissing=False");
-                connection.Open();
-            }
-            catch (SQLiteException ex)
-            {
-                Console.WriteLine($"Ошибка доступа к базе: {_DBname}. Исключение: : {ex.Message}");
-            }
-        }
-
         static void addEmployer(string _DBname) // метод добавления сотрудника
         {
-            
-            
-
             string _name, _sex, _position;
             int _year, _month, _day;
             SQLiteConnection connection = new SQLiteConnection("Data Source=" + _DBname + ";Version=3; FailIfMissing=False");
             connection.Open();
-            SQLiteCommand command = new SQLiteCommand( $"INSERT INTO Employer (Name, Age, Position) VALUES (@_name,@_age, @_position)", connection);
+            SQLiteCommand command = new SQLiteCommand($"INSERT INTO Employer (Name, Age, Position) VALUES (@_name,@_age, @_position)", connection);
 
             Console.WriteLine("Введите имя сотрудника");
             _name = Console.ReadLine();
-            
+
             do // сделали цикл ввода
             {
                 Console.WriteLine("Введите пол сотрудника: M - мужчина или W - женщика");
@@ -51,19 +32,19 @@ namespace Employer_LIST_HOMEWORK
             {
                 Console.WriteLine("Введите год рождения сотрудника: ");
                 _year = int.Parse(Console.ReadLine());
-            } while (_year < 1950  || _year > 2009);
+            } while (_year < 1950 || _year > 2009);
 
             do
             {
                 Console.WriteLine("Введите месяц рождения сотрудника ( от 1 до 12 ): ");
                 _month = int.Parse(Console.ReadLine());
-            } while (_month < 0 || _month >= 12);
+            } while (_month < 0 || _month > 12);
 
             do
             {
                 Console.WriteLine("Введите день рождения сотрудника ( от 1 до 31 ): ");
                 _day = int.Parse(Console.ReadLine());
-            } while (_day < 0 || _day >= 31);
+            } while (_day < 0 || _day > 31);
 
             var dateOfBirthday = new DateTime(_year, _month, _day);
 
@@ -81,28 +62,36 @@ namespace Employer_LIST_HOMEWORK
             command.Parameters.AddWithValue("@_name", _name);
             command.Parameters.AddWithValue("@_age", _age);
             command.Parameters.AddWithValue("@_position", _position);
+          //  command.Parameters.AddWithValue("@_position", _sex);
 
-            
+
             connection.Close();
-            
+
         }
+        static void printEmployers()
+        {
+
+        }
+
 
         static void Main(string[] args)
         {
 
-            SQLiteConnection connection = new SQLiteConnection();
-            SQLiteCommand command = new SQLiteCommand();
-            EmployerList employer1 = new EmployerList();
-            DBConnect("Employer.db");
-            command = new SQLiteCommand(connection)
-            {
-                CommandText = "CREATE TABLE IF NOT EXISTS [Employer]([id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE," + // создали БД
-                   " [Name], [Age] INTEGER, [Position] TEXT);"
-            };
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=Employer.db; ");
+            connection.Open();
+
+            var querry = @"CREATE TABLE IF NOT EXISTS [Employer]
+                              (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+                              Name NVARCHAR,  
+                              Age INTEGER, 
+                              Position NVARCHAR)";
+
+            var cmd = new SQLiteCommand(querry, connection);
             //command.ExecuteNonQuery();
+            connection.Close();
             Console.WriteLine("Таблица создана");
             addEmployer("Employer.db");
-            
+
             Console.ReadLine();
            
         }
